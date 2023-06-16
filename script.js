0b7a2652753ae57b38c2fbb25bfd647cc097fffd
Manjeet7087 <131547570+Manjeet7087@users.noreply.github.com>
2023-06-16 21:25:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function highlight() {
     //Write your code here
+	<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    .highlight {
+      color: green;
+    }
+  </style>
+</head>
+<body>
+  <p>
+    This is a <strong>sample</strong> paragraph with <strong>bold</strong> words.
+  </p>
+  <a id="highlightLink" href="#" onmouseover="highlight()" onmouseout="returnNormal()">Hover over me</a>
+
+  <script src="script.js"></script>
+</body>
+</html>
+
 
 
 }
@@ -7,6 +26,19 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
+function highlight() {
+  var boldWords = document.getElementsByTagName('strong');
+  for (var i = 0; i < boldWords.length; i++) {
+    boldWords[i].classList.add('highlight');
+  }
+}
+
+function returnNormal() {
+  var boldWords = document.getElementsByTagName('strong');
+  for (var i = 0; i < boldWords.length; i++) {
+    boldWords[i].classList.remove('highlight');
+  }
+}
 
     
-}
+
